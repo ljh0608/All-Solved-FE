7a7c140c6ca93b239ae546f48d23bcba1d714ad6
ljh0608 <m98m@catholic.ac.kr>
2023-04-28 16:16:36 +0900

    Init: 초기설정 / style

---
+++ src/styles/theme.js
@@ -0,0 +1,23 @@
+const fontSize = {
+
+  xxs: "0.625rem", // 10
+  xs: "0.75rem", // 12
+  sm: "0.875rem", // 14
+  md: "1rem", // 16
+  lg: "1.25rem",
+  xl: "1.5rem",
+  xxl:"1.75",
+};
+
+const colors = {
+    mainBlue: "#3A3E84",
+    mainLightGrey:"CACACA",
+    
+
+};
+
+export const theme = {
+  colors,
+
+  fontSize,
+};
